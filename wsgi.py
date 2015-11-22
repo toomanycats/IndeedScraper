13e13ee859ddb265a8ddacfe6bc5cd2ede13d1d1
toomanycats <dpcuneo@fastmail.fm>
2015-11-22 11:20:35 -0800

    fixed virtual path

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 import os
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+virtualenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'], 'virtenv', 'bin', 'activate_this.py')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
