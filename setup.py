f44a7cd72d56e62617d1626088eca594990c7fee
toomanycats <dpcuneo@fastmail.fm>
2015-11-22 09:21:44 -0800

    updated info

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='indeedscraper',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Daniel Cuneo',
+      author_email='dpcuneo@fastmail.fm',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
      )
