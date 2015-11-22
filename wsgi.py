c33c8e3ed5efacdc9ae92b3817dd2ac3d6ac7052
toomanycats <dpcuneo@fastmail.fm>
2015-11-22 11:34:57 -0800

    fixed import

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,7 @@ except IOError:
 # line, it's possible required libraries won't be in your searchable path
 #
 import indeed_scrape
-import web_version as application
+from web_version import app as application
 #
 # Below for testing only
 #
