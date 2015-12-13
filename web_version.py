e97eef9566a1a6f8c3b00f51741aa7bcf25d6ac6
toomanycats <dpcuneo@fastmail.fm>
2015-12-13 12:10:10 -0800

    added stat-counter script

--- web_version.py
+++ web_version.py
@@ -194,6 +194,25 @@ input_template = jinja2.Template('''
 
             <input type="submit" value="Submit" name="submit">
         </form>
+
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=10739395;
+var sc_invisible=1;
+var sc_security="0d075499";
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="free web stats"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/10739395/0/0d075499/1/"
+alt="free web stats"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
+
 </body>
 </html>''')
 
