0a6569c9d073c28a1c1b83f4fdcb83b4500e58d4
toomanycats <dpcuneo@fastmail.fm>
2016-01-23 11:37:01 -0800

    debug import after name change

--- wsgi.py
+++ wsgi.py
@@ -11,7 +11,7 @@ except IOError:
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-from web_version import app as application
+from keywordcounter import app as application
 #
 # Below for testing only
 #
